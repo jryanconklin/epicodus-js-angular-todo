c6e07ead6b4ec63c8b43cc3d622e7723eaae2a0b
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-16 15:49:41 -0700

    Add event emitter to project.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div class='container'>
     <h1>My First Angular 2 App</h1>
-    <h3 *ngFor="let currentTask of tasks"> {{ currentTask.description }}</h3>
+    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks"> {{ currentTask.description }}</h3>
   </div>
   `
 })
@@ -15,8 +15,15 @@ export class AppComponent {
     new Task("Create To-Do List app.", 0),
     new Task("Learn Kung Fu.", 1),
     new Task("Rewatch all the Lord of the Rings movies.", 2),
-    new Task("Do the laundry.", 3) 
+    new Task("Do the laundry.", 3)
   ];
+  doStuff(clickedTask: Task) {
+    if (clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not completed yet! Get to work!");
+    }
+  }
 }
 
 export class Task {
