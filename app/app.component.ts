c12bc383038d74858e4f31830bea44cc6f0c6ebe
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-16 15:29:05 -0700

    Add Task Class to Project

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,21 @@ import { Component } from '@angular/core';
   template: `
   <div class='container'>
     <h1>My First Angular 2 App</h1>
+    <h3 *ngFor="let currentTask of tasks"> {{ currentTask.description }}</h3>
   </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[] = [
+    new Task("Create To-Do List app.", 0),
+    new Task("Learn Kung Fu.", 1),
+    new Task("Rewatch all the Lord of the Rings movies.", 2),
+    new Task("Do the laundry.", 3) 
+  ];
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) { }
 }
