93bc345eefc7ae3b9aba39140661b99f910c8292
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-16 16:17:01 -0700

    Add Two Way Binding to App

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,16 @@ import { Component } from '@angular/core';
   template: `
   <div class='container'>
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks"> {{ currentTask.description }}</h3>
+    <h3 (click)="showDetails(currentTask)" *ngFor="let currentTask of tasks"> {{ currentTask.description }}</h3>
+    <h1>Edit Task</h1>
+    <div>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+    </div>
+    <div>
+      <label>Enter Task ID:</label>
+      <input [(ngModel)]="selectedTask.id">
+    </div>
   </div>
   `
 })
@@ -17,12 +26,9 @@ export class AppComponent {
     new Task("Rewatch all the Lord of the Rings movies.", 2),
     new Task("Do the laundry.", 3)
   ];
-  doStuff(clickedTask: Task) {
-    if (clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not completed yet! Get to work!");
-    }
+  selectedTask: Task = this.tasks[0];
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
 }
 
